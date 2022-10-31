4ad024f5681c01d71891d7571d7116be4d403f6a
Michael White <53944993+MykeWyte@users.noreply.github.com>
2022-10-31 00:40:11 -0400

    Update DJSetNode.java
    javadoc

--- DJSetNode.java
+++ DJSetNode.java
@@ -1,8 +1,18 @@
 package galacticBreakup;
 
 /**
- *
+ * This class serves as a node in a disjoint set. It has the ability to find 
+ * its root node in the set as well as join sets when given another node 
+ * 
  * @author Matthew Jacobs, Michael White
+ * @version 1.0.0
+ * file: DJSetNode.java
+ * Created October 2022
+ * Copyright Matthew Jacobs, Michael White
+ *
+ * Description: This class serves as a data structure to act as a node in a
+ * disjoint set. It can be used as a reference in another sets unionSet()
+ * method to join both sets
  */
 public class DJSetNode {
     // member values
@@ -14,6 +24,9 @@ public class DJSetNode {
     // constructors
     
     // functionally unnecessary, but follows powerpoint specifications
+    /**
+     * @description constructs default DJSetNode
+     */
     public static DJSetNode makeNode()
     {
         return new DJSetNode(); 
@@ -26,6 +39,9 @@ public class DJSetNode {
     }
     
     // other methods
+    /**
+     * @description returns the root node in this nodes Disjoint set
+     */
     public DJSetNode findSet()
     {
         if(this != this.par)
@@ -35,6 +51,11 @@ public class DJSetNode {
         return this.par;
     }
     
+    /**
+     * @description combines two disjoint sets into one set with a common head
+     * node which is returned.
+     * @param otherRep the other node to join sets with
+     */
     public DJSetNode union(DJSetNode otherRep)
     {
         // ensure the sets are being merged at the root to keep accurate ranks
